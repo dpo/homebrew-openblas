87bd2f5095d2a8d17d9c3501071a95100b514731
Dominique Orban <dominique.orban@gmail.com>
2018-01-11 14:34:50 +0100

    use ruby 2.3

--- ci/setup_travis_linux.sh
+++ ci/setup_travis_linux.sh
@@ -3,7 +3,7 @@ set -ev
 
 # Install LinuxBrew.
 sudo apt-get update
-sudo apt-get install build-essential curl git file python-setuptools m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+sudo apt-get install build-essential curl git file python-setuptools m4 texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
 
 HOME=/home/linuxbrew  # install linuxbrew here so we can install the glibc bottle
 sudo mkdir $HOME
